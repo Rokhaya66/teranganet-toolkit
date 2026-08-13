7fd5592abc2de6348b8297bdab10771f93f28f11
Salimata Sow <salimata.sw44@gmail.com>
2026-08-13 23:58:54 +0000

    Ajoute le chargement YAML de l'inventaire

--- teranganet/inventaire.py
+++ teranganet/inventaire.py
@@ -1,4 +1,12 @@
+"""Chargement de l'inventaire TerangaNet depuis un fichier YAML."""
+
+from pathlib import Path
+import yaml
+
+CHEMIN_DEFAUT = Path(__file__).resolve().parent.parent / "data" / "equipements.yaml"
 class Site:
+    """Représente un point de présence (POP) de TerangaNet."""
+
     def __init__(self, code, nom, latitude, longitude):
         self.code = code
         self.nom = nom
@@ -10,6 +18,8 @@ class Site:
 
 
 class Equipement:
+    """Représente un équipement réseau installé sur un site."""
+
     def __init__(self, nom, type_equipement, ip, statut, site, exterieur):
         self.nom = nom
         self.type_equipement = type_equipement
@@ -17,3 +27,17 @@ class Equipement:
         self.statut = statut
         self.site = site
         self.exterieur = exterieur 
+def charger_inventaire(chemin=CHEMIN_DEFAUT):
+    """Charge le YAML et renvoie (sites, equipements)."""
+    with open(chemin, encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+
+    sites = {}
+    for s in data["sites"]:
+        sites[s["code"]] = Site(s["code"], s["nom"], s["latitude"], s["longitude"])
+
+    equipements = []
+    for e in data["equipements"]:
+        equipements.append(Equipement(e["nom"], e["type"], e["ip"], e["statut"], sites[e["site"]], e["exterieur"]))
+
+    return sites, equipements
